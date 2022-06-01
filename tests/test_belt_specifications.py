22ff57818b2e74d675058e6caacb08bbedf41195
Rafael Bankosegger <rafael@bankosegger.at>
2022-06-01 10:24:09 +0200

    Some more belt testing and clean up todos

--- tests/test_belt_specifications.py
+++ tests/test_belt_specifications.py
@@ -79,6 +79,54 @@ class TestBeltSpecifications(unittest.TestCase):
         instance.solve()
         self.assertSetEqual(instance.models, models)
 
+    def test_one_belt_multiple_targets_with_same_order(self):
+
+        # Building `b1` has lowest order 1, i.e. must come first
+        # Building `b4` has highest order 4, i.e. must come last
+        # Buildings `b2`, `b3` have both the same order.
+        #   Two orders are possible between `b2` and `b3`
+        #   But they must be placed after `b1` and before `b4`
+
+        inmap = """
+            
+                            free((1,0)).    free((2,0)).    free((3,0)).
+            free((0,1)).    free((1,1)).    free((2,1)).    free((3,1)).
+
+            place(b1,(0,1)).
+            place_belt(belt,(1,1)).
+            place_belt(belt,(2,1)).
+            place_belt(belt,(3,1)).
+        """
+
+        specs = """
+            spec(bs1).
+            spec_size(bs1, 1, 1).
+        """
+
+        graph = """
+            supply_node_spec(b1, bs1).
+            supply_node_spec(b2, bs1).
+            supply_node_spec(b3, bs1).
+            supply_node_spec(b4, bs1).
+
+            supply_belt(belt).
+            supply_belt_connect_in_order(belt, b1, 1).
+            supply_belt_connect_in_order(belt, b2, 2).
+            supply_belt_connect_in_order(belt, b3, 2).
+            supply_belt_connect_in_order(belt, b4, 3).
+        """
+
+        models = {
+            frozenset({ 'place(b1,(0,1))', 'place(b2,(1,0))', 'place(b3,(2,0))', 'place(b4,(3,0))', 
+                        'place_belt(belt,(1,1))', 'place_belt(belt,(2,1))', 'place_belt(belt,(3,1))' }),
+            frozenset({ 'place(b1,(0,1))', 'place(b2,(2,0))', 'place(b3,(1,0))', 'place(b4,(3,0))', 
+                        'place_belt(belt,(1,1))', 'place_belt(belt,(2,1))', 'place_belt(belt,(3,1))' }),
+        }
+
+        instance = Factorio(inmap, specs, graph)
+        instance.solve()
+        self.assertSetEqual(instance.models, models)
+
     def test_belt_no_branching(self):
 
         inmap = """
@@ -110,6 +158,78 @@ class TestBeltSpecifications(unittest.TestCase):
             frozenset({ 'place(b1,(0,1))', 'place(b2,(2,1))',
                         'place_belt(belt,(0,2))', 'place_belt(belt,(1,2))', 'place_belt(belt,(2,2))' }),
         }
+
+        instance = Factorio(inmap, specs, graph)
+        instance.solve()
+        self.assertSetEqual(instance.models, models)
+
+    def test_belt_order_not_possible(self):
+
+        # Belt placement negative case: 
+        # Belt would have to branch to first reach order 2 and then go back to reach order 3 -> no answer sets possible!
+        inmap = """
+            free((0,0)).    free((1,0)).    free((2,0)).    free((3,0)).
+                            free((1,1)).
+                            free((1,2)).
+
+            place(b1,(0,0)).
+            place(b2,(1,2)).
+            place(b3,(3,0)).
+        """
+
+        specs = """
+            spec(bs1).
+            spec_size(bs1, 1, 1).
+        """
+
+        graph = """
+            supply_node_spec(b1, bs1).
+            supply_node_spec(b2, bs1).
+            supply_node_spec(b3, bs1).
+
+            supply_belt(belt).
+            supply_belt_connect_in_order(belt, b1, 1).
+            supply_belt_connect_in_order(belt, b2, 2).
+            supply_belt_connect_in_order(belt, b3, 3).
+        """
+
+        models = set()
+
+        instance = Factorio(inmap, specs, graph)
+        instance.solve()
+        self.assertSetEqual(instance.models, models)
+
+    def test_belt_no_branching_2(self):
+
+        # Belt placement negative case: 
+        # All have same order, but branching is not allowed -> no models!
+        inmap = """
+            free((0,0)).    free((1,0)).    free((2,0)).    free((3,0)).
+                            free((1,1)).
+                            free((1,2)).
+
+            place(b1,(0,0)).
+            place(b2,(1,2)).
+            place(b3,(3,0)).
+        """
+
+        specs = """
+            spec(bs1).
+            spec_size(bs1, 1, 1).
+        """
+
+        graph = """
+            supply_node_spec(b1, bs1).
+            supply_node_spec(b2, bs1).
+            supply_node_spec(b3, bs1).
+
+            supply_belt(belt).
+            supply_belt_connect_in_order(belt, b1, 1).
+            supply_belt_connect_in_order(belt, b2, 1).
+            supply_belt_connect_in_order(belt, b3, 1).
+        """
+
+        models = set()
 
         instance = Factorio(inmap, specs, graph)
         instance.solve()
@@ -209,6 +329,51 @@ class TestBeltSpecifications(unittest.TestCase):
         instance = Factorio(inmap, specs, graph)
         instance.solve()
         self.assertSetEqual(instance.models, models)
+
+    def test_two_belts_no_touching(self):
+
+        # Make sure that one belt cannot end in another!
+
+        inmap = """
+            free((0,0)).    free((1,0)).
+            free((0,1)).    free((1,1)).
+            free((0,2)).    free((1,2)).
+
+            place(b1,(0,0)).
+            place(b3,(1,2)).
+        """
+
+        specs = """
+            spec(bs1).
+            spec_size(bs1, 1, 1).
+        """
+
+        graph = """
+            supply_node_spec(b1, bs1).
+            supply_node_spec(b2, bs1).
+            supply_node_spec(b3, bs1).
+            supply_node_spec(b4, bs1).
+
+            supply_belt(belt1).
+            supply_belt_connect_in_order(belt1, b1, 1).
+            supply_belt_connect_in_order(belt1, b2, 2).
+
+            supply_belt(belt2).
+            supply_belt_connect_in_order(belt2, b3, 1).
+            supply_belt_connect_in_order(belt2, b4, 2).
+        """
+
+        models = {
+            frozenset({ 'place(b1,(0,0))', 'place(b2,(0,2))', 'place(b3,(1,2))', 'place(b4,(1,0))',
+                        'place_belt(belt1,(0,1))', 'place_belt(belt2,(1,1))' }),
+            frozenset({ 'place(b1,(0,0))', 'place(b2,(1,1))', 'place(b3,(1,2))', 'place(b4,(0,1))',
+                        'place_belt(belt1,(1,0))', 'place_belt(belt2,(0,2))' }),
+        }
+
+        instance = Factorio(inmap, specs, graph)
+        instance.solve()
+        self.assertSetEqual(instance.models, models)
+
 
     def test_belt_on_axis(self):
 
@@ -420,18 +585,5 @@ class TestBeltSpecifications(unittest.TestCase):
         self.assertSetEqual(instance.models, models)
 
 
-    # TODO: bugfix: one belt cannot end into another
-    # TODO: add requirements.txt
-    # TODO: Belt placement negative case: belt would have to branch to first reach order 2 and then go back to reach order 3 -> no answer sets possible!
-    # TODO: Test placement along belt with to buildings of same order (can be switched)
 
-    # TODO: Watch out that multiple touching points dont screw up the order
-    #           Construct example souch that the belt touches the following in order
-    #            a) touch point of order 1
-    #            b) multi-touch-point of order 3
-    #            c) touch point of order 2
-    #            d) another multi-touch-point of order 3
-    ###########
-    # Later: two inserters with belt inbetween may reduce to one inserter
-    # Later: Needs power source as input specification
-
+    
---
+++ todo.md
@@ -0,0 +1,17 @@
+# Bugs
+
+One belt cannot end into another
+
+Watch out that multiple touching points dont screw up the order
+ 	-> Construct example such that the belt must touch the following in order
+    		a) touch point of order 1
+    		b) multi-touch-point of order 3
+    		c) touch point of order 2
+    		d) another multi-touch-point of order 3
+
+
+# Factorio-specifics
+
+    * Two inserters with belt inbetween may reduce to one inserter
+    * Electric power source as input specification, with reachable-within-distance constraint
+	
